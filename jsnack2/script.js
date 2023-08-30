eb5a811c7b89bfd769a3c224e8cd3dba6bf3373e
Francesco-Munafo <acquamarea1@gmail.com>
2023-08-30 11:55:37 +0200

    Add jsnack2 while solution

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,6 +3,7 @@ L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
+/*
 let userWord1 = prompt('Scrivi una parola a caso');
 let userWord2 = prompt("Scegli un'altra parola");
 
@@ -10,4 +11,30 @@ if (userWord1.length > userWord2.length) {
     alert(userWord2 + " " + userWord1);
 } else {
     alert(userWord1 + " " + userWord2);
+}
+*/
+
+// WHILE SOLUTION
+
+let i = 1;
+let userWordList = [];
+
+while (i <= 2) {
+    const userWord = prompt('Inserisci una parola')
+    userWordList.push(userWord)
+    i++
+
+}
+if (userWordList[0].length > userWordList[1].length) {
+
+    alert('La parola' + " " + userWordList[0] + " " + "è più lunga")
+
+} else if (userWordList[0].length < userWordList[1].length){
+
+    alert('La parola' + " " + userWordList[1] + " " + "è più lunga")
+
+} else {
+
+    alert('La lunghezza delle parole è uguale')
+
 }
