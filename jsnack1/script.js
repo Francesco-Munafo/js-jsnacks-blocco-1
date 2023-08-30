2e2d09a89bb2d3eac969569e83681defbbd87059
Francesco-Munafo <acquamarea1@gmail.com>
2023-08-30 10:51:29 +0200

    Add jsnack1 while solution

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,6 +3,8 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
 
+/*
+
 let userNumber1 = prompt('Scegli un numero');
 let userNumber2 = prompt('Scegli un altro numero');
 
@@ -10,4 +12,30 @@ if (userNumber1 > userNumber2) {
     alert(userNumber1);
 } else {
     alert(userNumber2);
+}
+*/
+
+
+// WHILE SOLUTION
+
+let i = 1;
+const whileNumberList = []
+
+while (i <= 2) {
+    const whileUserNumber = Number(prompt('Scegli un numero'))
+
+    if (isNaN(whileUserNumber)) {
+        i--
+    } else {
+        whileNumberList.push(whileUserNumber)
+        i++
+    }
+}
+
+if (whileNumberList[0] > whileNumberList[1]) {
+
+    alert(whileNumberList[0])
+    
+} else {
+    alert(whileNumberList[1])
 }
